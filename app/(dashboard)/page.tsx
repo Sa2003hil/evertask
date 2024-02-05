1ea294fd807949f06558258b2bacc13f11ba2050
Sahil <Sahil2003@gmail.com>
2024-02-05 12:54:43 +0530

    Feat : New Ui components added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,24 @@
+import SadFace from "@/components/icons/SadFace";
+import { Alert, AlertTitle } from "@/components/ui/alert";
 import { Skeleton } from "@/components/ui/skeleton";
 import { wait } from "@/lib/wait";
-import { UserButton, currentUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
 import React, { Suspense } from "react";
+import  prisma from "@/lib/prisma";
+import CreateCollectionButton from "@/components/CreateCollectionButton";
 
 export default async function Home() {
   return (
+  <>
    <Suspense fallback={<WelcomeMsgFallback  />}>
     <WelcomeMsg/>
    </Suspense>
+   <Suspense fallback={<div>Loading Collections...</div>}>
+    <CollectionList/>
+   </Suspense>
+  
+  </>
+
   );
 }
 
@@ -18,7 +29,7 @@ async function WelcomeMsg(){
     return <div>Not logged in</div>
   }
   return (
-   <div className="flex w-full">
+   <div className="flex w-full mb-12">
     <h1 className="text-4xl font-bold">Welcome,<br/> {user?.firstName}</h1>
    </div>
   );
@@ -26,11 +37,36 @@ async function WelcomeMsg(){
 
 function WelcomeMsgFallback() {
   return (
-    <div className="flex w-full">
+    <div className="flex w-full mb-12">
       <h1 className="text-4xl flex flex-col gap-3 font-bold">
         <Skeleton className="w-[170px] h-[36px]" />
         <Skeleton className="w-[170px] h-[36px]" />
       </h1>
     </div>
   );
+}
+
+
+async function CollectionList(){
+  const user = await currentUser();
+  const collections = await prisma?.collection.findMany({
+    where:{
+      userId: user?.id
+    }
+  });
+
+  if(collections?.length === 0){
+    return(
+      <div className="flex flex-col gap-5">
+        <Alert>
+          <SadFace/>
+          <AlertTitle>There are no collections yet!</AlertTitle>
+          <AlertTitle>Create a collection to get started</AlertTitle>
+        </Alert>
+        <CreateCollectionButton/>
+      </div>
+
+    )
+  }
+
 }
