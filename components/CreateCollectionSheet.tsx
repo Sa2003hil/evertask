921cda38f06a9ab0fb100be250115039b707d39d
Sahil <Sahil2003@gmail.com>
2024-02-13 18:03:14 +0530

    Feat : Functionality added in sidebar+Fix : BugFix

--- components/CreateCollectionSheet.tsx
+++ components/CreateCollectionSheet.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from './ui/sheet';
+import { useForm } from "react-hook-form";
+import { createCollectionSchema, CreateCollectionSchemaType } from '@/schema/createCollection';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './ui/form';
+import { Input } from './ui/input';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select';
+import { cn } from '@/lib/utils';
+import { CollectionColors } from '@/lib/constants';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+
+
+
+
 
 interface Props {
     open: boolean;
@@ -7,6 +21,16 @@ interface Props {
 }
 
 const CreateCollectionSheet = ({ open, onOpenChnage }: Props) => {
+
+    const form = useForm<CreateCollectionSchemaType>({
+        resolver: zodResolver(createCollectionSchema),
+        defaultValues: {},
+    })
+
+    const onSubmit = (data: CreateCollectionSchemaType) => {
+        console.log('Submitted', data);
+    }
+
     return (
         <Sheet open={open} onOpenChange={onOpenChnage}>
             <SheetContent>
@@ -18,6 +42,61 @@ const CreateCollectionSheet = ({ open, onOpenChnage }: Props) => {
                         Collections are a way to group your tasks
                     </SheetDescription>
                 </SheetHeader>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-4 flex flex-col "
+                    >
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Personal" {...field} />
+                                    </FormControl>
+                                    <FormDescription>Collection name</FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="color"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Color</FormLabel>
+                                    <FormControl>
+                                        <Select onValueChange={(color) => field.onChange(color)}>
+                                            <SelectTrigger className={cn("w-full h-8 text-white", CollectionColors[field.value as CollectionColors])}>
+                                                <SelectValue placeholder="color" className='w-full h-8' />
+                                            </SelectTrigger>
+                                            <SelectContent className='w-full'>
+                                                {Object.keys(CollectionColors).map((color) => (
+                                                    <SelectItem key={color} value={color} className={
+                                                        cn(`w-full h-8 rounded-md my-1 text-white focous:text-white focus:font-bold focus:ring-2 focus:ring-inset dark:focus:ring-white focus:px-8`, CollectionColors[color as CollectionColors])
+                                                    }>
+                                                        {color}
+                                                    </SelectItem>
+                                                ))}
+                                            </SelectContent>
+                                        </Select>
+                                    </FormControl>
+                                    <FormDescription>Select a color for your collection</FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+
+                        />
+
+
+                    </form>
+                </Form>
+                <div className='flex flex-col gap-3 mt-4'>
+                    <Separator />
+                    <Button onClick={form.handleSubmit(onSubmit)}>Confirm</Button>
+                </div>
             </SheetContent>
         </Sheet>
     )
---
+++ schema/createCollection.ts
@@ -0,0 +1,12 @@
+import { CollectionColors } from '@/lib/constants';
+import {z} from 'zod';
+
+export const createCollectionSchema = z.object({
+    name:z.string().min(4,{
+        message:"Collection name should be at least 4 characters long"
+    }),
+    color:z.string().refine(color=>Object.keys(CollectionColors).includes(color))
+})
+
+
+export type CreateCollectionSchemaType=z.infer<typeof createCollectionSchema>
